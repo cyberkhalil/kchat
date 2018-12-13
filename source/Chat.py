30e596af1b00acc2c61af4ff1f5f3c2c34fea2dd
khalil2535 <kkhalil2535@gmail.com>
2018-12-13 14:14:51 +0200

    add "\n" for messages rename ChatProtocol class to KChatProtocol document KchatProtocol class add some documentation to Client class

--- source/Chat.py
+++ source/Chat.py
@@ -2,38 +2,43 @@ from socket import *  # library for sockets
 from threading import Thread  # import Thread class
 
 
-class ChatProtocol:
+class KChatProtocol:
     chat_port = 1234  # the server used port
-    connect_command = "#conn"
-    username_command = "#user"
-    users_list_command = "#ulst"
-    help_command = "#help"
-    exit_command = "#exit"
-    help_docs = "Welcome to KChat protocol docs \n The commands starts with '#' symbol " \
+    connect_command = "#conn"  # used in the first time to ensure from the connection.
+    username_command = "#user"  # used to change the username of a client.
+    users_list_command = "#ulst"  # used to request users names who connected to same server.
+    help_command = "#help"  # used to show the manual help page for the kchat.
+    exit_command = "#exit"  # used to close the connection and exit.
+    help_docs = "Welcome to KChat protocol docs \n    The commands starts with '#' symbol " \
                 "and the messages starts with '$' symbol \n         commands list :- \n     #conn : This " \
                 "command is used to connect to the server.\n     #user : This command is used to set a new " \
                 "username.\n     #ulst : This command is used to see the other users connected to the server.\n" \
                 "     #help : This command is used to see this docs.\n     #exit : This command is used to exit" \
-                "the connection from server."
-    connect_command_b = connect_command.encode()
-    username_command_b = username_command.encode()
-    users_list_command_b = users_list_command.encode()
-    help_command_b = help_command.encode()
-    exit_command_b = exit_command.encode()
-    help_docs_b = help_docs.encode()
+                "the connection from server.\n    " \
+                "Messages starts with '$' symbol, To send a message to all just put $your_message .\n" \
+                ""  # the help documents for the Kchat protocol.
+
+    connect_command_b = connect_command.encode()  # the connect_command encoded
+    username_command_b = username_command.encode()  # the username_command encoded
+    users_list_command_b = users_list_command.encode()  # the users_list_command encoded
+    help_command_b = help_command.encode()  # the help_command encoded
+    exit_command_b = exit_command.encode()  # the exit_command encoded
+    help_docs_b = help_docs.encode()  # the help_docs encoded
 
 
 class Client:
-    username = None
-    client_socket = None
-    isAlive = False
+    """Client class is an implementation for KChat protocol in the client side, using socket and TCP connection"""
+
+    username = None  # the username of the client
+    client_socket = None  # the socket we use to communicate with the server
+    isAlive = False  # the state of the connection with the server
 
     def connect(self):
         try:
             # create client socket
             self.client_socket = socket(AF_INET, SOCK_STREAM)  # create socket for chatting
-            self.client_socket.connect(('127.0.0.1', ChatProtocol.chat_port))  # connect with server for chatting
-            self.client_socket.send(ChatProtocol.connect_command_b)
+            self.client_socket.connect(('127.0.0.1', KChatProtocol.chat_port))  # connect with server for chatting
+            self.client_socket.send(KChatProtocol.connect_command_b)
             self.isAlive = True
             return 1
         except ConnectionRefusedError:
@@ -45,7 +50,7 @@ class Client:
         if not self.isAlive:
             print("Socket isn't alive so you can't send exit message using exit()")
             return
-        self.client_socket.send(ChatProtocol.exit_command_b)
+        self.client_socket.send(KChatProtocol.exit_command_b)
         self.client_socket.close()
         self.isAlive = False
 
@@ -76,15 +81,15 @@ class Client:
             return
 
         if msg.startswith("$") & ("$" in msg[1:]):
-            msg = list(msg[1:])
+            msg = list(msg[1:-1])
             index = msg.index("$")
             msg[index] = ":"
             msg = "".join(msg)
             return msg
         elif msg.startswith("#"):
-            if msg == ChatProtocol.exit_command_b:
+            if msg == KChatProtocol.exit_command_b:
                 self.exit()
-            elif msg == ChatProtocol.username_command_b:
+            elif msg == KChatProtocol.username_command_b:
                 self.send_username()
 
         return self.receive_from_server()
@@ -93,13 +98,13 @@ class Client:
         if not self.isAlive:
             print("Socket isn't alive so you can't request_members()")
             return
-        self.client_socket.sendall(ChatProtocol.users_list_command_b)
+        self.client_socket.sendall(KChatProtocol.users_list_command_b)
 
     def send_username(self):
         if not self.isAlive:
             print("Socket isn't alive so you can't send_username()")
             return
-        self.client_socket.sendall(ChatProtocol.username_command_b + self.username.encode())
+        self.client_socket.sendall(KChatProtocol.username_command_b + self.username.encode())
 
 
 """
@@ -119,7 +124,7 @@ class Server:
         for client in self.clients_list:
             try:
                 client_socket = client[1]
-                client_socket.send(str(msg).encode())
+                client_socket.send(str(msg + "\n").encode())
                 print('sent to ' + client[0])
             # TODO expect exception type
             except:
@@ -132,13 +137,13 @@ class Server:
 
         # If didn't receive connect command from client then return and stop the thread
         client_msg = client_socket.recv(1024).decode().strip()
-        while not client_msg == ChatProtocol.connect_command:
+        while not client_msg == KChatProtocol.connect_command:
             print("Client " + client_address + " didn't send connect command")
             client_msg = client_socket.recv(1024).decode().strip()
 
         print(client_address + '> established new connection')
 
-        client_socket.send(ChatProtocol.username_command_b)  # request username
+        client_socket.send(KChatProtocol.username_command_b + "\n".encode())  # request username
         client_msg = client_socket.recv(1024).decode().strip()
         while not client_msg.startswith("#user"):
             print("Client " + client_address + " didn't get username from " + client_address)
@@ -150,18 +155,16 @@ class Server:
         client_info = [username, client_socket]
         self.clients_list.append(client_info)
         try:
-            client_socket.send(("$server$ Welcome " + username + ", you can start chatting  ^_^").encode())
+            client_socket.send(("$server$ Welcome " + username + ", you can start chatting  ^_^\n").encode())
             while 1:
-                msg = client_socket.recv(1024).decode().strip()
-                if msg == "":
-                    continue
-
-                elif msg.startswith("#"):
-                    if msg == ChatProtocol.exit_command:
+                msg = client_socket.recv(1024).decode()
+                if msg.startswith("#"):
+                    msg = msg.strip()
+                    if msg == KChatProtocol.exit_command:
                         break
-                    elif msg == ChatProtocol.help_command:
-                        client_socket.send(ChatProtocol.help_docs_b)
-                    elif msg.startswith(ChatProtocol.username_command):
+                    elif msg == KChatProtocol.help_command:
+                        client_socket.send(KChatProtocol.help_docs_b + "\n".encode())
+                    elif msg.startswith(KChatProtocol.username_command):
                         username = msg[5:].strip()
                         print(client_address + '> changing his username to ' + username)
                         self.clients_list.remove(client_info)
@@ -174,6 +177,11 @@ class Server:
                     msg = msg[1:]
                     print(client_info[0] + "> sending " + msg)
                     self.send_to_all("$" + client_info[0] + "$ " + msg)
+                else:
+                    if msg == "":  # telnet sends a lot of these..
+                        continue
+                    else:
+                        print('weird message from ' + username + ' "' + msg + '"')
         except:
             print(client_info[0] + "> has error")
 
@@ -184,7 +192,7 @@ class Server:
     def run(self):
         server_socket = socket(AF_INET, SOCK_STREAM)  # using TCP
 
-        server_socket.bind(('', ChatProtocol.chat_port))  # open & listen to chat port
+        server_socket.bind(('', KChatProtocol.chat_port))  # open & listen to chat port
         server_socket.listen(1)
 
         print("The chat server is ready")
